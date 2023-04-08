0a84caae6eb8373b1304dc1c43b824ee9e9d992b
cesar <ocesaralmeida@gmail.com>
2023-04-08 19:40:02 -0300

    started my movies project, created header, mainMenu, MovieItem and MoviesList components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { HeadeContainer } from "../styles/HeaderContainer";
+import { MainMenu } from "./MainMenu"; 
+
+
+export const Header = () => (
+    <HeadeContainer>
+        <h1>My Movies</h1>
+
+        <MainMenu />
+    </HeadeContainer>
+);
---
+++ src/styles/HeaderContainer.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const HeadeContainer = styled.header`
+    text-align: center;
+    padding: 50px 0;
+`;
