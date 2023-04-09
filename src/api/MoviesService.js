42eb7dc13323fc82473d282e6d1350977abca0b9
cesar <ocesaralmeida@gmail.com>
2023-04-09 09:46:19 -0300

    fetch movies API and list most popular movies

---
+++ src/api/MoviesService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API_KEY = '642da665ac91bdaedfaf0863aaed1c28';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'));
+    }
+};
